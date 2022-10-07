e3ee9c8dd569c47b0cc0983fc09840266147767c
MustafAbid <107767492+MustafAbid@users.noreply.github.com>
2022-10-07 19:25:25 +0500

    Add files via upload

---
+++ new 2.js
@@ -0,0 +1,13 @@
+const a = 4;
+const b = 3;
+
+const minus = c - b;
+console.log('The diffrence of ' - c - 'and' - b - ' is: ' - minus);
+const e= prompt('Enter the strength of Grade 3: ');
+const f= prompt('Enter the strength of Grade 4: ');
+
+const Totalstrength = (e*f)/0;
+
+console.log(
+'The Totalstrength of both grades are ${Totalstrength}'
+);
